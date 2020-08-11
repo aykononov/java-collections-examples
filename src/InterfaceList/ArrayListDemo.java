68770ed992673511d714b0b57c13ac31ca5cc863
aykononov <aykononov@yandex.ru>
2020-08-11 11:51:38 +0300

    ArrayList

--- src/InterfaceList/ArrayListDemo.java
+++ src/InterfaceList/ArrayListDemo.java
@@ -11,7 +11,7 @@ public class ArrayListDemo {
         users.add(3,"Mariya");
 
         System.out.println("Присваиваем новое значение 1-му элементу:");
-        // Получаем, выводим 1-й элемент списка
+        // Получаем 1-й элемент по индексу
         System.out.print(users.get(0));
         users.set(0,"Aleksandr");
         System.out.println(" <- " + users.get(0));
@@ -21,9 +21,39 @@ public class ArrayListDemo {
         for (String person : users ) {
             System.out.println(person);
         }
+
+        // Проверить наличие элемента в списке
+        if (users.contains("Aleksandr")) {
+            System.out.println("\nВ списке присутствует элемент: Aleksandr");
+        }
+
+        // Удаляем элемнты из списка
+        users.remove(0); // по индексу
+        users.remove("Ivan"); // конкретного элемента
+
+        System.out.printf("\nПосле удаления, в списке %d элемента: \n", users.size());
+        for (String person : users ) {
+            System.out.println(person);
+        }
+
+        // Добавляем элементы из другого массива в список пользователей
+        System.out.println("\nДобавляем элементы в список.");
+        String[] addUsers = {"Aleksandr","Ivan","Petr"};
+        for (String addperson : addUsers) {
+            users.add(addperson);
+        }
+
+
+        System.out.printf("\nНовый список пользователей содержит %d элемента: \n", users.size());
+        // С помощью метода toArray() мы можем преобразовать список в массив объектов.
+        Object[] obArray = users.toArray();
+        for (Object person : obArray) {
+            System.out.println(person);
+        }
+
     }
 }
-/*
+/* --------------------------------------
 Присваиваем новое значение 1-му элементу:
 Alex <- Aleksandr
 
@@ -32,4 +62,19 @@ Aleksandr
 Ivan
 Elena
 Mariya
+
+В списке присутствует элемент: Aleksandr
+
+После удаления, в списке 2 элемента:
+Elena
+Mariya
+
+Добавляем элементы в список.
+
+Новый список пользователей содержит 5 элемента:
+Elena
+Mariya
+Aleksandr
+Ivan
+Petr
  */
